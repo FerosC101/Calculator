9d0c5fb5271462a9dd36fd7810e85d5b9b8e299e
KC Dimayuga <kcdimayuga651@gmail.com>
2025-05-10 04:22:54 +0800

    Add files via upload

---
+++ CalculatorAppLab4/CalculatorAppLab4/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CalculatorAppLab4
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmCalculator
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "frmCalculator";
+            this.Load += new System.EventHandler(this.frmCalculator_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_clearEntry;
+        private System.Windows.Forms.Button button_addition;
+        private System.Windows.Forms.Button button_subtraction;
+        private System.Windows.Forms.Button button_multiplication;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button_equals;
+        private System.Windows.Forms.Button button_point;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
